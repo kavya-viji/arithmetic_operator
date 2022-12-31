da483484ec9ab32e1b9a313280767e62c29482f9
Admin <kavyak6999@gmail.com>
2022-12-31 11:31:42 +0530

    arithmetic operator

--- operator/operator.js
+++ operator/operator.js
@@ -1,9 +1,18 @@
 var x = parseInt(prompt("enter x value"));
 var y = parseInt(prompt("enter y value"));
-//var z=x+y;
-//var z=x-y;
-//var z=x*y;
-//var z=x/y;
-//var z=x%y;
-//document.write(z);
-alert (z)
+var z=x+y;
+document.write(z);
+document.write("<br>");
+var z=x-y;
+document.write(z);
+document.write("<br>");
+var z=x*y;
+document.write(z);
+document.write("<br>");
+var z=x/y;
+document.write(z);
+document.write("<br>");
+var z=x%y;
+document.write(z);
+document.write("<br>");
+//alert (z)
